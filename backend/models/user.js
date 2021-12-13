d910407cb44fcc3ff28970d2f1787557a0ffb65c
Frazonar <snip.coloured@gmail.com>
2021-12-13 19:20:38 +0200

    update user schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,7 +1,22 @@
 const mongoose = require('mongoose');
+const isEmail = require('validator/lib/isEmail');
 const { urlRegex } = require('../utils/constants');
 
 const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (v) => isEmail(v),
+      message: 'Wrong email format',
+    },
+  },
+  password: {
+    type: String,
+    minlength: 8,
+    required: true,
+  },
   name: {
     type: String,
     minlength: 2,

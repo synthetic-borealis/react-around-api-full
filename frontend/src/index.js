a75dd2e8a0c9eaa5b7fd9ce8fc9afa9054107262
Frazonar <snip.coloured@gmail.com>
2022-06-09 12:22:06 +0300

    frontend:chore: switch to new backend server

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,7 +10,7 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={'/react-around-api-full/'}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
